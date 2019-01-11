ffe01be4c228050a1d5075ba15aaba1d5ca8f13b
fendoudebb <15317832520@163.com>
2019-01-11 16:52:30 +0800

    batch insert fake data

--- application/route.php
+++ application/route.php
@@ -3,14 +3,20 @@
 use think\Route;
 
 Route::get('/id', 'index/Index/index');
+Route::any('/admin/insert/batch', 'admin/BatchInsert/batchInsert',['method'=>'get|post']);
 
 Route::post('/admin/login', 'admin/Login/login');
 
 
 //---Topic api start---
-Route::post('/admin/topic', 'admin/Topic/topicInfo');
+Route::post('/admin/topic', 'admin/Topic/topicList');
 Route::post('/admin/topic/add', 'admin/TopicAdd/addTopic');
 //---Topic api end---
+
+
+//---Post api start---
+Route::post('/admin/post', 'admin/Post/postList');
+//---Post api end---
 
 return [
 
