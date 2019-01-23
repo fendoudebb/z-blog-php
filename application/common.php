919473c94c8bfc57ce67a9dd5b1bfe31fce73985
fendoudebb <15317832520@163.com>
2019-01-23 22:29:19 +0800

    compress html function

--- application/common.php
+++ application/common.php
@@ -22,3 +22,26 @@ function doGet($url) {
     if ($error) throw new Exception('请求发生错误：' . $error);
     return $result;
 }
+
+function compressHtml($string) {
+    $string = str_replace("\r\n", '', $string); //清除换行符
+    $string = str_replace("\n", '', $string); //清除换行符
+    $string = str_replace("\t", '', $string); //清除制表符
+    $pattern = array(
+        "/> *([^ ]*) *</", //去掉注释标记
+        "/[\s]+/",
+        "/<!--[^!]*-->/",
+        "/\" /",
+        "/ \"/",
+        "'/\*[^*]*\*/'"
+    );
+    $replace = array(
+        ">\\1<",
+        " ",
+        "",
+        "\"",
+        "\"",
+        ""
+    );
+    return preg_replace($pattern, $replace, $string);
+}
