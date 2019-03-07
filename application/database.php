322206203290e5d2382b0969f95c95e73a01277a
fendoudebb <15317832520@163.com>
2019-03-07 22:52:12 +0800

    change db to mongo

--- application/database.php
+++ application/database.php
@@ -9,7 +9,7 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'maisiji',
+    'database'        => 'zblog',
     // 用户名
     'username'        => '',
     // 密码
