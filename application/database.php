2de19af4d93f0ff3d81d0acf4f1f4211a55ab2b9
fendoudebb <15317832520@163.com>
2019-03-13 13:36:39 +0800

    z-blog

--- application/database.php
+++ application/database.php
@@ -9,7 +9,7 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'zblog',
+    'database'        => 'z-blog',
     // 用户名
     'username'        => '',
     // 密码
