1370e603f0c79ec93c009ba72c68a14c0b8e804f
fendoudebb <15317832520@163.com>
2019-06-29 21:32:46 +0800

    filter ip

--- application/index/controller/QueryIp.php
+++ application/index/controller/QueryIp.php
@@ -9,10 +9,20 @@ class QueryIp extends Base {
 
     public function queryIp() {
         $ip = strval(input("post.ip"));
+
+        $queryTime = date('Y-m-d H:i:s');
+        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+            return json(['code' => -1, 'msg' => "[$queryTime]：不合法的IP地址~"]);
+        }
+
+        if(!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE)) {
+            return json(['code' => -1, 'msg' => "[$queryTime]：内网地址~"]);
+        }
+
         $ipUtil = new IpUtil();
         $address = $ipUtil->getAddressByIp($ip);
         if ($address == null) {
-            return json(['code' => -1, 'msg' => '查询失败，请再试一次~']);
+            return json(['code' => -1, 'msg' => "[$queryTime]：查询失败，请再试一次~"]);
         }
         return json(['code' => 200, 'address' => $address]);
     }
