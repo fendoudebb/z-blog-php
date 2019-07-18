e440d97ffd8b22be34f490cb79340acdee3f327a
fendoudebb <15317832520@163.com>
2019-07-18 21:04:15 +0800

    rank controller

---
+++ application/admin/controller/Rank.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\admin\controller;
+
+
+class Rank extends BaseRoleNormal {
+
+    public function pv() {
+
+    }
+
+    public function likes() {
+
+    }
+
+    public function comments() {
+
+    }
+
+}
