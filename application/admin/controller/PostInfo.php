7f385d14209010bf17eb251aec23393a442da3a0
fendoudebb <15317832520@163.com>
2019-11-23 11:04:08 +0800

    update by postId replace by _id

--- application/admin/controller/PostInfo.php
+++ application/admin/controller/PostInfo.php
@@ -5,28 +5,23 @@ namespace app\admin\controller;
 
 use app\common\config\ResCode;
 use app\common\util\Mongo;
-use MongoDB\BSON\ObjectId;
-use think\Db;
 
 class PostInfo extends BaseRoleNormal {
 
     public function postInfo() {
-        $postId = input('post.postId');
+        $postId = intval(input('post.postId'));
         if (!isset($postId)) {
             $this->log(ResCode::MISSING_PARAMS_POST_ID);
             return $this->fail(ResCode::MISSING_PARAMS_POST_ID);
         }
-        if (strlen($postId) !== 24) {
-            $this->log(ResCode::ILLEGAL_ARGUMENT_POST_ID);
-            return $this->fail(ResCode::ILLEGAL_ARGUMENT_POST_ID);
-        }
         $findPostCmd = [
             'find' => 'post',
             'filter' => [
-                '_id' => new ObjectId($postId)
+                'postId' => $postId
             ],
             'projection' => [
-                '_id' => 1,
+                '_id' => 0,
+                'postId' => 1,
                 'title' => 1,
                 'postStatus' => 1,
                 'content' => 1,
@@ -41,12 +36,6 @@ class PostInfo extends BaseRoleNormal {
             return $this->fail(ResCode::POST_DOES_NOT_EXIST);
         }
         $post = $postCmdArr[0];
-        $objectId = $post->_id;
-        if (is_object($objectId) && method_exists($objectId, '__toString')) {
-            $id = $objectId->__toString();
-            $post->id = $id;
-            unset($post->_id);
-        };
         return $this->res($post);
     }
 
